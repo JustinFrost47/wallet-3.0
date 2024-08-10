e6cf83795c4442452a97faaa0b0064beda4b77df
JustinFrost47 <rinojonathanarul@gmail.com>
2024-08-10 23:46:02 +0530

    generation  of menmonic  phrase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import GeneratePhrase from './PhraseGeneration/GeneratePhrase'
 import WalletDetails from './WalletDetails'
 
 export default function Home() {
+  
+  const [phrase, setPhrase] = useState(localStorage.getItem('mnemonic'))
+  
   return (
     
     <div className='w-screen h-screen bg-slate-800  '>
@@ -9,8 +13,15 @@ export default function Home() {
         <div className="container px-12 py-8 flex flex-col items-center justify-center ">
         <div className='text-white text-4xl my-12' >Web Crypto Wallet</div>
 
-            <GeneratePhrase/>
+          {phrase ? (
+
             <WalletDetails/>
+
+          ): (
+            <GeneratePhrase/>
+          )}
+            
+            
         </div>
     </div>
   )
--- src/pages/PhraseGeneration/GeneratePhrase.tsx
+++ src/pages/PhraseGeneration/GeneratePhrase.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button } from '../../components/ui/button'
 import CopyArea from './CopyArea'
 
@@ -7,16 +8,33 @@ import { generateMnemonic } from 'bip39';
 
 export default function GeneratePhrase() {
 
-  const createPhrase = () => {
+    const [phrase, setPhrase] = useState('')
 
-    const mnemonic = generateMnemonic();
-    console.log('Generated Mnemonic:', mnemonic);
-  }
+    const createPhrase = () => {
 
-  return (
-    <>
-            <Button onClick={createPhrase}>Generate Mnemonic</Button>
-            <CopyArea/>
-    </>
-  )
+        const mnemonic = generateMnemonic();
+        localStorage.setItem('mnemonic', mnemonic)
+        setPhrase(mnemonic)
+        // console.log('Generated Mnemonic:', mnemonic);
+    }
+
+    const refreshPage = () => {
+        window.location.reload();
+    }
+
+    return (
+
+        <>
+            {phrase ?
+                (<>
+                <CopyArea wordSequence={phrase} />
+                <Button onClick={refreshPage}>Continue</Button>
+                </>)
+                : (
+                    <Button onClick={createPhrase}>Generate Mnemonic</Button>
+                )}
+
+        </>
+
+    )
 }
