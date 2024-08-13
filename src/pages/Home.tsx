cc9d51e71eedb792ba4fcc5efa1325dee34a5090
JustinFrost47 <rinojonathanarul@gmail.com>
2024-08-13 22:57:48 +0530

    made the full screen follow the background color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   return (
 
-    <div className='w-screen h-screen bg-slate-800  '>
+    <div className='w-screen min-h-screen bg-slate-800  '>
 
       <div className="container px-12 py-8 flex flex-col items-center justify-center ">
         <div className='text-white text-4xl my-12' >Web Crypto Wallet</div>
