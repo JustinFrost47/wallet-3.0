068720c26e95bc11f8d76a7e08dbdee3041ca980
JustinFrost47 <rinojonathanarul@gmail.com>
2024-08-17 23:55:32 +0530

    added option to add multiple keys for solana

--- src/pages/KeyGeneration/SolanaKeys.tsx
+++ src/pages/KeyGeneration/SolanaKeys.tsx
@@ -22,56 +22,72 @@ interface SolKeys {
 
 export default function SolanaKeys({ phrase }: SolanaKeyProp) {
 
-    const [solKeys, setSolKeys] = useState<SolKeys>({publicKey: "", privateKey: ""})
+    const [count, setCount] = useState(1)
 
-    const generateKeyPair = (mnemonic: string) => {
+    const [solKeys, setSolKeys] = useState<SolKeys[]>([])
 
-        let solanaPublicKey="" 
-        let solanaPrivateKey =""
+    const generateKeyPair = (mnemonic: string, pathCount: number = 1) => {
+
+
         const seed = mnemonicToSeedSync(mnemonic);
-        for (let i = 0; i < 4; i++) {
-            const path = `m/44'/501'/${i}'/0'`; // This is the derivation path
-            const derivedSeed = derivePath(path, seed.toString("hex")).key;
-            // const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
-            //console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
-            // setSolKeys(Keypair.fromSecretKey(secret).publicKey.toBase58())
 
-            const keypair = nacl.sign.keyPair.fromSeed(derivedSeed);
-            const secretKey = keypair.secretKey;
-            // const publicKey = keypair.publicKey;
+        const path = `m/44'/501'/${pathCount-1}'/0'`; // This is the derivation path
+        const derivedSeed = derivePath(path, seed.toString("hex")).key;
+        // const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+        //console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
+        // setSolKeys(Keypair.fromSecretKey(secret).publicKey.toBase58())
 
-           solanaPublicKey = Keypair.fromSecretKey(secretKey).publicKey.toBase58();
-            const solanaPrivateKeyHex = Buffer.from(secretKey).toString('hex')
+        const keypair = nacl.sign.keyPair.fromSeed(derivedSeed);
+        const secretKey = keypair.secretKey;
+        // const publicKey = keypair.publicKey;
 
-
-            //hex to base58 conversion
-            solanaPrivateKey = bs58.encode(Buffer.from(solanaPrivateKeyHex, 'hex'));
+        const solanaPublicKey = Keypair.fromSecretKey(secretKey).publicKey.toBase58();
+        const solanaPrivateKeyHex = Buffer.from(secretKey).toString('hex')
 
 
-
-
-
-
-        }
-        setSolKeys({
+        //hex to base58 conversion
+        const solanaPrivateKey = bs58.encode(Buffer.from(solanaPrivateKeyHex, 'hex'));
+        setSolKeys([
+            ...solKeys,
+            {
+                publicKey: solanaPublicKey,
+                privateKey: solanaPrivateKey,
+            }
+        ])
+        console.log(            {
             publicKey: solanaPublicKey,
             privateKey: solanaPrivateKey,
         })
+        console.log(solKeys)
+
     }
+
+    const incrementKeys = () => {
+        setCount(count + 1)
+        generateKeyPair(phrase, count+1)
+    }
+
 
 
     return (
         <>
 
 
-            {solKeys.publicKey && solKeys.privateKey  ? (
+            {solKeys.length > 0 ? (
 
 
                 <>
-                    <div >
-                        <img className="h-8 w-8 " src="https://cryptologos.cc/logos/solana-sol-logo.png?v=032" alt="Solana" />
-                    </div>
-                    <WalletDetails publicKey={solKeys.publicKey} privateKey={solKeys.privateKey} />
+                    <img className="h-8 w-8 " src="https://cryptologos.cc/logos/solana-sol-logo.png?v=032" alt="Solana" />
+                    {solKeys.map((solKey, i) => (
+
+                            <div key={i} >
+
+                            
+                            <WalletDetails publicKey={solKey.publicKey} privateKey={solKey.privateKey} />
+                            </div>
+
+                    ))}
+                    <Button className="my-8" onClick={incrementKeys}> Add Keys </Button>
                 </>
 
             ) : (
